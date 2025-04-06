de6c719b3d4fadebf340f58e464dc4a69c68e2ec
CRIMSONHydra <53392400+CRIMSONHydra@users.noreply.github.com>
2025-04-06 18:57:17 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,42 @@
 import React, { useState } from 'react'
 import Logo from '../assets/placeholder-logo.jpg';
 import { Link } from 'react-router-dom';
+import './navbar.css';
 
 function Navbar() {
 
   const [input, setInput] = useState("Search...");
   const [dropDown, setDropDown] = useState(false);
 
+  function clearSearch() {
+    if(input == "Search...") setInput("");
+  }
+
   return (
     <nav>
-      <div>
-        <Link to="/"><img src={Logo} /></Link>
-      </div>
+      <div className="nav-left">
+        <div>
+          <Link to="/"><img src={Logo} /></Link>
+        </div>
 
-      <div>
-        <Link to="/">Home</Link>
-      </div>
+        <div className='nav-links'>
+          <Link to="/">Home</Link>
+        </div>
 
-      <div>
-        <input type='text' value={input} onChange={e => setInput(e.target.value)}/>
+        <div>
+          <input type='text' value={input} onClick={clearSearch} onChange={e => setInput(e.target.value)}/>
+        </div>
       </div>
+      
 
-      <div>
-        <div onClick={()=> setDropDown(!dropDown)}>User</div>
+      <div className="user-dropdown" onClick={() => setDropDown(!dropDown)}>
+        User
         {dropDown && (
-          <div>
-            <Link to="/profile">Profile</Link>
-            <Link to="/cart">Cart</Link>
-            <Link to="/wishlist">Wish List</Link>
-          </div>
+          <ul className="dropdown-menu">
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/cart">Cart</Link></li>
+            <li><Link to="/wishlist">Wish List</Link></li>
+          </ul>
         )}
       </div>
     </nav>
