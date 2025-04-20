718121a386ca7ef4ede2c7e39623a5b6d4513afe
CRIMSONHydra <53392400+CRIMSONHydra@users.noreply.github.com>
2025-04-20 19:37:15 +0530

    added wishlist

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+import ProductCard from '../components/ProductCard';
+import { getExchangeRate } from '../utils/getExchangeRate';
+
+import { useAuth } from '../context/useAuth';
 
 function Cart() {
+  const { userData} = useAuth();
+  const [products, setProducts] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [rate, setRate] = useState(1);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(console.error);
+  }, []);  
+
+  useEffect(() => {
+    if (!userData?.cart || products.length === 0) return;
+
+    setFiltered(
+      products.filter(product => userData.cart.includes(product.id))
+    );
+
+    async function getRate() {
+      const r = await getExchangeRate();
+      setRate(r);
+    }
+    getRate();
+  }, [products, userData?.cart]);  
+
+  const totalPrice = (filtered.reduce((sum, item) => sum + item.price, 0) * rate).toFixed(2);
+
   return (
-    <div>Cart</div>
+    <div className='container'>
+      <h2>🛒 User Cart</h2>
+
+      <div className='product-grid'>
+        {filtered.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+
+      {filtered.length > 0 && (
+        <div className='price'>
+          Total Price: ₹ {totalPrice}
+        </div>
+      )}
+    </div>
   )
 }
 
--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+import ProductCard from '../components/ProductCard';
+
+import { useAuth } from '../context/useAuth';
 
 function WishList() {
+  const { userData} = useAuth();
+  const [products, setProducts] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(console.error);
+  }, []);  
+
+  useEffect(() => {
+    if (!userData?.wishlist || products.length === 0) return;
+
+    setFiltered(
+      products.filter(product => userData.wishlist.includes(product.id))
+    );
+  }, [products, userData?.wishlist]);  
+
   return (
-    <div>WishList</div>
+    <div className='container'>
+      <h2>User Wishlist</h2>
+
+      <div className='product-grid'>
+        {filtered.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+
+      <div className='count'>Total items: {filtered.length}</div>
+    </div>
   )
 }
 
